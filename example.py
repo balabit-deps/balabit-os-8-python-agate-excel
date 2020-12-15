8c83bdee105a8a5b1f19f3d4d053d1fd9f40c826
Kócsó Balázs <kocsob@balabit.hu>
2020-12-15 20:43:10 +0100

    Import Upstream version 0.2.3

---
+++ example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import agate
+import agateexcel  # noqa
+
+table = agate.Table.from_xls('examples/test.xls')
+
+print(table)
+
+table = agate.Table.from_xlsx('examples/test.xlsx')
+
+print(table)
+table.print_table()
